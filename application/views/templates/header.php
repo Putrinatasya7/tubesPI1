e7d6f0e2daa139bd97983302c00b09ec8f671248
patrisiatbn <patrisiatbn@gmail.com>
2021-05-26 14:19:05 +0700

    edit controller Barang

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -53,7 +53,7 @@
         </li>
         <!-- barang -->
         <li class="nav-item">
-          <a class="nav-link  " href="<?php echo base_url('') ?>">
+          <a class="nav-link  " href="<?php echo base_url('Barang') ?>">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>credit-card</title>
@@ -72,6 +72,7 @@
             <span class="nav-link-text ms-1">Barang</span>
           </a>
         </li>
+
         <!-- supplier -->
         <li class="nav-item">
           <a class="nav-link" href="<?php echo base_url('Supplier') ?>">
