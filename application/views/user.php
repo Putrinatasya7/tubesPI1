bf05012b02f1314a04c62a2e5326d3a48f93ec4d
patrisiatbn <patrisiatbn@gmail.com>
2021-05-26 21:21:52 +0700

    add user view

---
+++ application/views/user.php
@@ -0,0 +1,68 @@
+ 
+<main class="main-content mt-1 border-radius-lg">
+
+<!-- Navbar -->
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+  <div class="container">
+<div class="box">
+  <div class="box-header">
+    <h3 class="box-title">Add User</h3>
+    <div class="pull-right">
+       
+      </a>
+    </div>
+  </div>
+  
+  <div class="box-body">
+    <div class="row">
+      <div class="form-group">
+
+        <label>Name</label>
+        <input type="text" name="fullname" class="form-control">
+  </div>
+
+
+  <div class="row">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="username" class="form-control">
+</div>
+<div class="row">
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" name="password" class="form-control">
+      </div>
+
+      <div class="row">
+      <div class="form-group">
+        <label>Password Confirmation</label>
+        <input type="password" name="passwordconf" class="form-control">
+      </div>
+
+      <div class="row">
+      <div class="form-group">
+        <label>Alamat</label>
+        <textarea name="text" name="address" class="form-control">
+        </textarea>
+      </div>
+    </div>
+      <div class="form-group">
+        <button type="submit" class=" btn btn-success btn-flat">Save</button>
+        <button type="Reset" class= "btn btn-flat"> Reset</button>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+  </div>
+</div>
+</div>
+
+</section>
+</nav>
+</main>
