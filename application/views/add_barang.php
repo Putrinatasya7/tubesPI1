1f7ec511489c07b5eaf7244fb038f77e72bcc3ff
patrisiatbn <patrisiatbn@gmail.com>
2021-06-01 03:32:25 +0700

    edit action icon in add_barang view | add modal for item detail

--- application/views/add_barang.php
+++ application/views/add_barang.php
@@ -16,7 +16,7 @@
                       <th class="align-middle text-center text-secondary text-s ps-2">Kode Barang</th>
                       <th class="align-middle text-center text-secondary text-s">Jumlah Barang</th>
                       <th class="align-middle text-center text-secondary text-s">Kategori</th>
-                      <th class="align-middle text-center text-secondary text-s" colspan="2">Action</th>
+                      <th class="align-middle text-center text-secondary text-s" colspan="3">Action</th>
                       <th></th>
                     </tr>
                   </thead>
@@ -27,8 +27,9 @@
                       <td class="align-middle text-center text-sm">10</span></td>
                       <td class="align-middle text-center text-sm">perabot</span></td>
                       
-                      <td class="align-middle text-center"><button class="badge badge-sm btn bg-gradient-info" data-toggle="modal" data-target="#editModal">Edit</button></td>
-                      <td class="align-middle text-center" ><button class="badge bagde-sm btn bg-gradient-danger" data-toggle="modal" data-target="#deleteModal">Delete</button></td>
+                      <td class="align-middle text-center"><button class="badge badge-sm btn bg-gradient-warning" data-toggle="modal" data-target="#editModal"><i class="fa fa-pen top-0" title="Edit"></i></button></td>
+                      <td class="align-middle text-center"><button class="badge badge-sm btn bg-gradient-info" data-toggle="modal" data-target="#detailBarang"><i class="fa fa-search top-0" title="Detail"></i></button></td>
+                      <td class="align-middle text-center" ><button class="badge bagde-sm btn bg-gradient-danger" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash top-0" title="Delete"></i></button></td>
                       <td></td>
                       <!-- <td><a href="<?= base_url(); ?>admin/update/<?= $object->id; ?>"><span class="badge badge-sm bg-gradient-info">Edit</span></a></td>
                       <td><a href="<?= base_url(); ?>admin/delete/<?= $object->id; ?>"><span class="badge badge-sm bg-gradient-danger" onclick="return confirm('Anda yakin ingin menghapus akun ini?')">Delete</span></a></td> -->
@@ -49,40 +50,40 @@
   
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLabel">Add Item</h5>
-                <!-- <button type="button" data-dismiss="modal"><span aria-hidden="true">&times;</span></button> -->
+                <a type="button" data-dismiss="modal"><span aria-hidden="true"><i class="fa fa-times top-0"></i></span></a>
             </div>
   
             <div class="modal-body">
                 <form action="<?= base_url(); ?>admin/insert" method="post">
   
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="name">Item names</label>
               <input type="text" class="form-control" name="name" id="name" placeholder="Enter name ..." required>
             </div>
   
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="code">Item Code</label>
               <input type="text" class="form-control" disabled>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="total">Total</label>
               <input type="text" class="form-control" name="total" id="total" placeholder="Enter jumlah ..." required>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="caregory">Category</label>
               <select class="form-control" id="active" name="active">
                   <option value="Active">Kategory 1</option>
                   <option value="Inactive">Kategori 2</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="brand">Brand</label>
               <select class="form-control" id="active" name="active">
                   <option value="Active">Merk 1</option>
                   <option value="Inactive">Merk 2</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="price">Price</label>
               <input type="text" class="form-control" name="price" id="price" placeholder="Enter kategori ..." required>
             </div>
@@ -106,7 +107,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Edit Items Detail</h5>
-          <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+          <a type="button" data-dismiss="modal"><span aria-hidden="true"><i class="fa fa-times top-0"></i></span></a>
         </div>
   
         <form role="form" action="<?php echo base_url('Controller_Warehouse/update') ?>" method="post" id="updateForm">
@@ -114,33 +115,33 @@
           <div class="modal-body">
             <div id="messages"></div>
   
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="edit_item_name">Item names</label>
               <input type="text" class="form-control" id="edit_item_name" name="edit_item_name" placeholder="Enter nama barang" autocomplete="off">
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="code">Item code</label>
               <input type="text" class="form-control" disabled>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="edit_total">Total</label>
               <input type="text" class="form-control" name="edit_total" id="edit_total" placeholder="Enter jumlah ..." required>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="category">Category</label>
               <select class="form-control" id="active" name="active">
                   <option value="Active">Kategory 1</option>
                   <option value="Inactive">Kategori 2</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="edit_brand">Brand</label>
                 <select class="form-control" id="active" name="active">
                   <option value="Active">Merk 1</option>
                   <option value="Inactive">Merk 2</option>
                 </select>
             </div>
-            <div class="form-group">
+            <div class="form-group input-group-sm">
               <label for="edit_price">Price</label>
               <input type="text" class="form-control" id="edit_price" name="edit_price" placeholder="Enter nama barang" autocomplete="off">
             </div>
@@ -158,6 +159,74 @@
   
         </form>
   
+  
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+  <!-- Items Detail -->
+  <div class="modal fade" tabindex="-1" aria-labelledby="detailBarangLabel" role="dialog" id="detailBarang">
+    <div class="modal-dialog modal-xl"  role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Items Detail</h5>
+          <a type="button" data-dismiss="modal"><span aria-hidden="true"><i class="fa fa-times top-0"></i></span></a>
+        </div>
+  
+        <form role="form" action="<?php echo base_url('Controller_Warehouse/update') ?>" method="post" id="updateForm">
+  
+          <div class="modal-body">
+          <div class="col-md-12">
+          <div class="row">
+          <div class="col-md-2">
+              <div class="avatar avatar-xxl position-relative">
+              <img src="<?php echo base_url();?>asset/bootstrap/img/bruce-mars.jpg" alt="..." class="w-100 position-relative z-index-2 border-radius-lg shadow-sm">
+              </div>
+          </div>
+          <div class="col-md-5">
+            <div class="form-group">
+              <label for="edit_item_name">Item names</label>
+              <input type="text" class="form-control" id="edit_item_name" name="edit_item_name" placeholder="Enter nama barang" autocomplete="off">
+            </div>
+            
+            <div class="form-group">
+              <label for="code">Item code</label>
+              <input type="text" class="form-control" disabled>
+            </div>
+            
+            <div class="form-group">
+              <label for="edit_total">Total</label>
+              <input type="text" class="form-control" name="edit_total" id="edit_total" placeholder="Enter total ..." required>
+            </div>
+            </div>
+
+            <div class="col-md-5">
+            <div class="form-group">
+              <label for="category">Category</label>
+              <select class="form-control" id="active" name="active">
+                  <option value="Active">Kategory 1</option>
+                  <option value="Inactive">Kategori 2</option>
+                </select>
+            </div>
+            <div class="form-group">
+              <label for="edit_brand">Brand</label>
+                <select class="form-control" id="active" name="active">
+                  <option value="Active">Merk 1</option>
+                  <option value="Inactive">Merk 2</option>
+                </select>
+            </div>
+            <div class="form-group">
+              <label for="edit_price">Price</label>
+              <input type="text" class="form-control" id="edit_price" name="edit_price" placeholder="Harga barang" autocomplete="off">
+            </div>
+          </div>
+          </div>
+          </div>
+         
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </form>
   
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
