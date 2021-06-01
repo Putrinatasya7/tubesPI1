c615bfc00f06056860b9044988d9b4ef56455c05
Putri Natasya <80021731+Putrinatasya7@users.noreply.github.com>
2021-06-01 12:15:09 +0700

    Menambahkan beberapa fungsi untuk tampilan request

--- application/controllers/Request.php
+++ application/controllers/Request.php
@@ -5,20 +5,36 @@ class Request extends CI_Controller {
 
   public function addIn() {
     $data['title'] = "Request";
+    $data['subtitle'] = "Request Barang Masuk";
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('requestIn');
+    $this->load->view('templates/footer');
+  }
+
+  public function addOut() {
+    $data['title'] = "Request";
+    $data['subtitle'] = "Request Barang Keluar";
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('requestOut');
+    $this->load->view('templates/footer');
+  }
+  public function addReqIn() {
+    $data['title'] = "Request";
     $data['subtitle'] = "Create Request Barang Masuk";
 
     $this->load->view('templates/header', $data);
     $this->load->view('addRequestIn');
     $this->load->view('templates/footer');
   }
-
-  public function addOut() {
+  public function addReqOut() {
     $data['title'] = "Request";
-		$data['subtitle'] = "Create Request Barang Keluar";
+    $data['subtitle'] = "Create Request Barang Keluar";
 
     $this->load->view('templates/header', $data);
     $this->load->view('addRequestOut');
     $this->load->view('templates/footer');
   }
-  
 }
+
