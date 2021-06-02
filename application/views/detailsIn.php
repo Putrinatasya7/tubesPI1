cf96453611e7cf08439e76e52cdb2d9810bbe7a2
Putri Natasya <80021731+Putrinatasya7@users.noreply.github.com>
2021-06-02 13:11:20 +0700

    Add new file detailsIn.php

---
+++ application/views/detailsIn.php
@@ -0,0 +1,68 @@
+
+    <div class="container-fluid py-4">
+      <div class="card">
+        <div class="card-header pb-0 p-3">
+          <div class="row">
+            <div class="col">
+              <h5>Request No. </h5>
+              <h6>Request By.</h6>
+            </div>
+            <div class="col">
+              <h6>Date</h6>
+              <h6>To. </h6>
+            </div>
+          </div>
+        </div>
+        <div class="card-footer pb-2 p-3">
+        <div class="col">
+            <table class="table align-items-center mb-0">
+          <thead>
+            <tr>
+              <th class="text-secondary text-s">Item</th>
+              <th class="align-middle text-center text-secondary text-s">Supplier</th>
+              <th class="align-middle text-center text-secondary text-s">Jumlah</th>
+              <th class="align-middle text-center text-secondary text-s">Kategori</th>
+              <th class="align-middle text-center text-secondary text-s">Price</th>
+            </thead>
+          </tr>
+            <tr>
+              <td class="text-xs font-weight-bold">Dunlop SP Sport LM705 185/70 R14 Ban Mobil [Tahun 2021]</td>
+              <td class="align-middle text-center text-xs font-weight-bold">PT.Maju Jaya</td>
+              <td class="align-middle text-center text-xs font-weight-bold">5</td>
+              <td class="align-middle text-center text-xs font-weight-bold">Suku Cadang</td>
+              <td class="align-middle text-center text-xs font-weight-bold">Rp.50000</td>
+              </tr>
+          <tr>
+              <td class="text-xs font-weight-bold">Dunlop Direzza DZ101 195/50 R16 - Produksi 2021</td>
+              <td class="align-middle text-center text-xs font-weight-bold">PT Bersama Maju</td>
+              <td class="align-middle text-center text-xs font-weight-bold">2</td>
+              <td class="align-middle text-center text-xs font-weight-bold">Suku Cadang</td>
+              <td class="align-middle text-center text-xs font-weight-bold">Rp.50000</td>
+          </tr>
+        </table>
+      </thead>
+      <div class="card-footer pb-0 p-3">
+        <div class="card">
+          <div class="card-header pb-0 p-3">
+            <div class="row">
+              <div class="row mt-2">
+                <div class="col-md-3">
+                  <div class="input-group input-group-sm">
+                    <span class="input-group-text">Total Item</span>
+                    <input type="text" aria-label="First name" class="form-control">
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="input-group input-group-sm">
+                    <span class="input-group-text">Subtotal</span>
+                    <span class="input-group-text">IDR</span>
+                    <input type="text" aria-label="First name" class="form-control">
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer pb-2 p-3">
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
