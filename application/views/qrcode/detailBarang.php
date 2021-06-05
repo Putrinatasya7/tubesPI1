23be92f4d8b7653a9df82e66b8e212599c1b4f5d
Jessica Wong <wongjessica124@gmail.com>
2021-06-05 23:54:48 +0700

    QR Code file changes

---
+++ application/views/qrcode/detailBarang.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/bootstrap/img/apple-icon.png') ?>">
+  <link rel="icon" type="image/png" href="<?php echo base_url('asset/bootstrap/img/icon.png') ?>">
+  <title>
+    Inventory System
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-icons.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="<?php echo base_url('asset/bootstrap/css/soft-ui-dashboard.css?v=1.0.2') ?>" rel="stylesheet" />
+  <!-- for Modal -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+
+</head>
+
+<body>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card mb-4">
+            <div class="card-header pb-0">
+              <h5>Detail Barang</h5>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="col-md-12">
+                <div class="row p-4">
+                  <div class="col-md-4">
+                    <img src="<?php echo base_url(); ?>asset/pict/barang/<?= $barang['pict']; ?>" alt="..." class="w-100 position-relative z-index-2 border-radius-lg shadow-sm">
+                  </div>
+                  <div class="col-md-8">
+                    <h6 class="font-weight-bolder"><?= $barang['barang_id']; ?></h6>
+                    <h5 class="font-weight-bolder text-info text-gradient"><?= $barang['barang']; ?></h5>
+                    <h6><?= $barang['merk']; ?> <span class="font-weight-normal">| <?= $barang['category']; ?></span></h6>
+                    <h6>Rp <?= $barang['harga']; ?></h6>
+                    <h6 class="font-weight-normal">Stock : <?= $barang['stock']; ?></h6>
+                    <div class="row mt-5">
+                      <div class="col-md-6">
+                        <h6>Date Created</h6>
+                        <h6 class="font-weight-normal">Selasa, 2 Juni 2021</h6>
+                      </div>
+                      <div class="col-md-6">
+                        <h6>Date Modified</h6>
+                        <h6 class="font-weight-normal">Selasa, 2 Juni 2021</h6>
+                      </div>
+                    </div> <!-- /row mt-5-->
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </main>
+</body>
+
+</html>
---
+++ application/views/qrcode/detailInvoice.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/bootstrap/img/apple-icon.png') ?>">
+  <link rel="icon" type="image/png" href="<?php echo base_url('asset/bootstrap/img/icon.png') ?>">
+  <title>
+    Inventory System
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-icons.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="<?php echo base_url('asset/bootstrap/css/nucleo-svg.css') ?>" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="<?php echo base_url('asset/bootstrap/css/soft-ui-dashboard.css?v=1.0.2') ?>" rel="stylesheet" />
+  <!-- for Modal -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+
+</head>
+
+<body>
+  <main>
+    <div class="container pt-5">
+      <div class="card">
+        <div class="card-header pb-0">
+          <div class="row">
+            <div class="col">
+              <h6 class="text-s font-weight-bolder"><span class="font-weight-normal pe-1">Invoice ID.</span> <?= $invoice[0]['invoice_no']; ?></h6>
+              <h6 class="text-sm"><span class="font-weight-normal pe-1">Invoice Date</span> <?= $invoice[0]['invoice_date']; ?></h6>
+            </div>
+            <div class="col">
+              <h6 class="text-sm"><span class="font-weight-normal pe-1">Receive Date</span> <?= $invoice[0]['received_at']; ?></h6>
+              <h6 class="text-sm"><span class="font-weight-normal pe-1">Receive by</span> <?= $invoice[0]['received_by']; ?></h6>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-body p-3">
+          <div class="row mt-1">
+            <div class="col-md-6">
+              <?php if ($invoice[0]['status_inv_in'] == "waiting for delivery") {
+                $bg = "bg-gradient-warning";
+              } elseif ($invoice[0]['status_inv_in'] == "received") {
+                $bg = "bg-gradient-success";
+              } ?>
+              <span class="badge badge-sm <?= $bg; ?>"><?= $invoice[0]['status_inv_in']; ?></span>
+            </div>
+          </div>
+
+          <form action="" method="post">
+            <div class="row">
+              <div class="table-responsive">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <td class="align-middle text-center text-xs font-weight-bold">Item</th>
+                      <?php if ($invoice[0]['req_category'] == "In") : ?>
+                    <td class="align-middle text-center text-xs font-weight-bold">Supplier</th>
+                    <?php endif; ?>
+                    <td class="align-middle text-center text-xs font-weight-bold">Qty</th>
+                      <?php if ($invoice[0]['req_category'] == "In") : ?>
+                    <td class="align-middle text-center text-xs font-weight-bold">Unit Price</th>
+                    <td class="align-middle text-center text-xs font-weight-bold">Amount</th>
+                    <?php endif; ?>
+                    <td class="align-middle text-center text-xs font-weight-bold">QR Code</th>
+                  </thead>
+                  <tbody>
+                    <?php $total_item = 0;
+                    $total_price = 0; ?>
+                    <?php foreach ($invoice as $inv) : ?>
+                      <tr>
+                        <td class="align-middle text-center text-xs"><?= $inv['barang']; ?></td>
+                        <?php if ($inv['req_category'] == "In") : ?>
+                          <td class="align-middle text-center text-xs"><?= $inv['supplier']; ?></td>
+                        <?php endif; ?>
+                        <td class="align-middle text-center text-xs"><?= $inv['qty']; ?></td>
+                        <?php if ($inv['req_category'] == "In") : ?>
+                          <td class="align-middle text-center text-xs">Rp <?= number_format($inv['harga_satuan'], 0, ',', '.'); ?></td>
+                          <td class="align-middle text-center text-xs">Rp <?= number_format($inv['harga_satuan'] * $inv['qty'], 0, ',', '.'); ?></td>
+                        <?php endif; ?>
+                        <td class="align-middle text-center text-xs"><img src="<?= base_url('asset/pict/qrcode/') . $inv['qrcode_barang']; ?>"></td>
+                      </tr>
+                      <?php if ($inv['req_category'] == "In") : ?>
+                        <?php $total_item += $inv['qty'];
+                        $total_price += $inv['harga_satuan'] * $inv['qty'] ?>
+                      <?php endif; ?>
+                    <?php endforeach; ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <?php if ($invoice[0]['req_category'] == "In") : ?>
+              <div class="row mt-5">
+                <div class="col-md-3">
+                  <div class="input-group input-group-sm">
+                    <span class="input-group-text">Total Item</span>
+                    <input type="text" aria-label="First name" class="form-control" value="<?= $total_item; ?>">
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="input-group input-group-sm">
+                    <span class="input-group-text">Subtotal</span>
+                    <span class="input-group-text">IDR</span>
+                    <input type="text" aria-label="First name" class="form-control" value="<?= number_format($total_price, 0, '.', '.'); ?>">
+                  </div>
+                </div>
+              </div>
+            <?php else : ?>
+              <div class="row mt-5">
+                <div class="col-md">
+                  <div class="form-group form-group-sm">
+                    <label for="exampleFormControlTextarea1">Alasan Pengambilan</label>
+                    <textbox class="form-control" id="exampleFormControlTextarea1" rows="3"><?= $invoice[0]['alasan_keluar']; ?></textbox>
+                  </div>
+                </div>
+              </div>
+            <?php endif; ?>
+            <div class="row">
+              <div class="col-md-12 mt-5 text-right">
+                <h6>Accepted By</h6>
+              </div>
+              <div class="col-md-12 ms-6 text-right">
+                <img src="<?= base_url('asset/signature/') . $invoice[0]['sign-img']; ?>" alt="" width="20%">
+              </div>
+            </div>
+
+
+
+          </form>
+        </div>
+
+
+      </div>
+    </div>
+
+
+  </main>
+</body>
+
+</html>
